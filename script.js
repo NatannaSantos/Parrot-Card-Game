a2b566ed9db1f388fca6592eb132221a89cf0a69
Natanna <natannacortez@gmail.com>
2021-11-05 14:38:12 -0300

    alteração JavaScript

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-prompt("Entre com a quantidade de cartas do jogo:");
+let numeroDeCartas = prompt("Entre com a quantidade de cartas do jogo (o número deve estar entre 4 e 14 e ser par):");
 
+if(numeroDeCartas!=='4' && numeroDeCartas!=='6'){
+    alert("numero inválido");
+}
